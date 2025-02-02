a5193a081ac3fd0df05836e799e7a44107a913ab
pezzaliapp <pezzalialessandro@gmail.com>
2025-02-02 18:55:26 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+// service-worker.js
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open("flexrentcalc-cache").then((cache) => {
+            return cache.addAll([
+                "./",
+                "./index.html",
+                "./style.css",
+                "./app.js",
+                "./manifest.json"
+            ]);
+        })
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
